74646fc2a85c5e1fde31cfe0aedd58616d215645
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-05-26 14:36:16 +0200

    added global setup file to automate whole process

---
+++ mac-setup.sh
@@ -0,0 +1,9 @@
+# Install apple command line tools
+xcode-select --install
+
+# Clone git repo containing config files
+git clone git@github.com:DannySeidel/mac-setup.git ~/.config-files
+
+# Run setup
+bash ~/.config-files/setup.sh
+
