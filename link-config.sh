da526067f80f1e9c21e7541e8966fec9097237dd
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-06-07 13:49:50 +0200

    added p10k config

--- link-config.sh
+++ link-config.sh
@@ -1,3 +1,4 @@
+ln -s ~/.config-files/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.config-files/.zshrc ~/.zshrc
 ln -s ~/.config-files/.gitconfig ~/.gitconfig
 ln -s ~/.config-files/.gitconfig-private ~/.gitconfig-private
