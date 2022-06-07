610fd5a62e784a8c48a8c7c6449ea2dbae81ed9f
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-06-07 14:14:59 +0200

    updated install script

--- install-software.sh
+++ install-software.sh
@@ -4,6 +4,10 @@
 # Install listed software via brew
 brew bundle --file ~/.config-files/Brewfile
 
+# Install p10k
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
 # Clear brew cache
 brew cleanup --prune=all
 
