9e3b413c6194c102d2aa7adfb8a1bbe65bbc3882
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-06-08 14:07:23 +0200

    updated link config script

--- link-config.sh
+++ link-config.sh
@@ -1,3 +1,7 @@
+mkdir -p ~/.vim/colors
+ln -s ~/.config-files/onehalf.vim ~/.vim/colors/onehalf.vim
+
+ln -s ~/.config-files/.vimrc ~/.vimrc
 ln -s ~/.config-files/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.config-files/.zshrc ~/.zshrc
 ln -s ~/.config-files/.gitconfig ~/.gitconfig
