f5c851903108acde5ea87bfc92266bd2fbd33e88
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-03-13 18:51:21 +0100

    updated shell config

--- .zshrc
+++ .zshrc
@@ -30,6 +30,9 @@ export LANGUAGE='en_US.UTF-8 git'
 
 export GPG_TTY=$(tty)
 
+printf "\n"
+neofetch
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
--- link-config.sh
+++ link-config.sh
@@ -4,6 +4,7 @@ ln -s ~/.config-files/init.vim ~/.config/nvim/init.vim
 ln -s ~/.config-files/.vimrc ~/.vimrc
 ln -s ~/.config-files/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.config-files/.zshrc ~/.zshrc
+ln -s ~/.config-files/.hushlogin ~/.hushlogin
 ln -s ~/.config-files/.gitconfig ~/.gitconfig
 ln -s ~/.config-files/.gitconfig-private ~/.gitconfig-private
 ln -s ~/.config-files/.gitconfig-work ~/.gitconfig-work
