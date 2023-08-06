764a3d2a5fdd339d92db00442f3d56a95abaaa9b
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-08-06 13:27:43 +0200

    add configure dock script

---
+++ configure-dock.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+
+defaults write com.apple.dock mineffect scale
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock tilesize -integer 48
+defaults write com.apple.dock largesize -float 64
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock autohide-delay -float 3
+
+killall Dock
