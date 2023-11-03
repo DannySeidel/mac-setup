e7ea63c13c06aad4c616deaff87b497714d6f0db
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-11-03 20:53:27 +0100

    add hidden menu bar in the tv app

--- scripts/configure-menubar.sh
+++ scripts/configure-menubar.sh
@@ -2,6 +2,10 @@
 
 echo "Configuring Menu bar..."
 defaults write NSGlobalDomain _HIHideMenuBar -bool false
-defaults write company.thebrowser.Browser AppleMenuBarVisibleInFullscreen -bool false
 
+# hide the Menu bar in the TV app and Arc
+defaults write com.apple.TV AppleMenuBarVisibleInFullscreen -bool false
+killall TV
+
+defaults write company.thebrowser.Browser AppleMenuBarVisibleInFullscreen -bool false
 killall Arc; open -a Arc
